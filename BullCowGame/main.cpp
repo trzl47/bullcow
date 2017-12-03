dad58ea82d855a30d9669646c68de38b143cc5df
trzl47 <brownty47@gmail.com>
2017-12-03 11:32:41 -0600

    small error handling added; some refactoring

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,23 +3,37 @@
 
 using namespace std;
 
-int getChallenge(); // get user Challenge
-bool checkChallenge(string); //validate Challenge input
+// start the game
 void PrintIntro(); // introduce the game
+
+void PlayGame();
+
+// player choose difficulty
+void challengePrompt(string); // prompt user for challenge selection
+string getChallenge(); // get user Challenge
+bool checkChallenge(string); //validate Challenge input
+
+// playing the game
 string getGuess(); // get user input
 bool checkGuess(string); // validate user input
+
+// play again?
+bool PlayAgain(); // ask player exit or play again
 
 
 //entry point for application
 int main() {
 	PrintIntro();
-	getChallenge();
+	
+	//getChallenge();
 
-	constexpr int player_turns = 5;
-	for (int i = 1; i <= 5; i++) {
-		getGuess();
-		cout << endl;
+	PlayGame();
+	while ((PlayAgain() == true)) {
+		PrintIntro();
+		PlayGame();
 	}
+	//PlayAgain();
+	return 0; // exit application
 }
 
 // introduce the game
@@ -30,39 +44,75 @@ void PrintIntro() {
 	return;
 }
 
-// get a guess from the player
-string getGuess() {
-	string Guess = "Enter your answer:";
-	getline(cin, Guess);
-	return Guess;
+void challengePrompt(string difficulties[]) {
+	#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
+
+	// display prompt
+	cout << "Choose your challenge level" << endl;
+	cout << difficulties << endl;
+
+	for (int i = 0; i < ARRAY_SIZE(difficulties); i++)
+	{
+		cout << difficulties[i] << endl;
+	};
 }
 
+string getChallenge() {
+	string difficulties[] = { "easy", "medium", "hard" };
 
-int getChallenge() {
-	// display prompt
-	cout << "Choose your Challenge" << endl
-		<< "1 - 3asy" << endl
-		<< "2 - Medium" << endl
-		<< "1 - Hard" << endl;
+	challengePrompt(difficulties); // display prompt
 
 	//user Challenge selection
 	string Challenge = "";
 	getline(cin, Challenge);
+	cout << "Selected " <<
 
 	//input validation
 	checkChallenge(Challenge);
+
+	return Challenge;
 }
 
 // validate user input for Challenge
-bool checkChallenge(string) {
+bool checkChallenge(string challenge) {
 	bool validChallenge = false;
-	//check user entered number
-	//check number entered is one of choices
+
 	return validChallenge;
+}
+
+void PlayGame() {
+	constexpr int player_turns = 5;
+	for (int i = 1; i <= 5; i++) {
+		// get a guess from the player
+		getGuess();
+		cout << endl;
+	}
+}
+
+string getGuess() {
+	string Guess = "";
+	cout << "Enter your answer:";
+	getline(cin, Guess);
+	cout << "You entered " << Guess << endl;
+	return Guess;
 }
 
 // validate user input for answer
 bool checkGuess() {
 	bool validAnswer = false;
 	return validAnswer;
+}
+
+// play again?
+bool PlayAgain() {
+	cout << "Would you like to play again? (y/n)" << endl;
+	string resp = "";
+	getline(cin, resp);
+	if ((resp[0] == 'y') || (resp[0] == 'Y')) {
+		return true;
+	}
+	else {
+		cout << "Thanks for playing!" << endl;
+		return false;
+	}
 }
